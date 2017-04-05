bde72b75c4269b6b500a8f912ef5bd98271b167b
TeemoGirl <348333967@qq.com>
2017-04-05 15:37:22 +0800

    change

--- main/js/demo.js
+++ main/js/demo.js
@@ -34,45 +34,4 @@ $(function(){
 	    }
 	})
 	
-	
-	//ajax实现文件下载
-	$('#downBtn').click(function(){
-		var tt = new Date().getTime();
-		var url = 'D://User/Documents/HBuilderProject/WebComponent/demo';
- 
-	/**
-	* 使用form表单来发送请求
-	* 1.method属性用来设置请求的类型——post还是get
-	* 2.action属性用来设置请求路径。
-	* 
-	*/
-	var form=$("<form>");//定义一个form表单
-	form.attr("style","display:none");
-	form.attr("target","");
-	form.attr("method","get");  //请求类型
-	form.attr("action",url);   //请求地址
-	$("body").append(form);//将表单放置在web中
- 
-
-	/*
-	 * input标签主要用来传递请求所需的参数：
-	 * name属性是传递请求所需的参数名.
-	 * value属性是传递请求所需的参数值.
-	 * 当为get类型时，请求所需的参数用input标签来传递，直接写在URL后面是无效的。
-	 * 当为post类型时，queryString参数直接写在URL后面，formData参数则用input标签传递
-	 * 有多少数据则使用多少input标签
-	 * */
-    var input1=$("<input>");
-    input1.attr("type","hidden");
-    input1.attr("name","tt");
-    form.append(input1);
- 
-    var input2=$("<input>");
-    input2.attr("type","hidden");
-    input2.attr("name","Carousel");
-    form.append(input2);
- 
-    form.submit();//表单提交
- })      
-	
 })
